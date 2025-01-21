b5504d27f7f4f0e249c4c00c01f002cef9db0228
Mokajin <mokajin@protonmail.com>
2025-01-21 11:05:03 +0400

    Intermediate changes, need to complete

--- src/generator.go
+++ src/generator.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "crypto/rand"
-    "math/big"
+	"crypto/rand"
+	"math/big"
 )
 
 // Generate bounded value between minimum and maximum
-func randIntBound(min, max int) int {
-    n, _ := rand.Int(rand.Reader, big.NewInt(int64(max)-int64(min)+1))
-    res := int64(min) + n.Int64()
-    return int(res)
+func randIntBound(minV, maxV int) int {
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(maxV)-int64(minV)+1))
+	res := int64(minV) + n.Int64()
+	return int(res)
 }
 
 // Generate value between 0 and max including boundaries
-func randInt(max int) int {
-    return randIntBound(0, max)
+func randInt(maxV int) int {
+	return randIntBound(0, maxV)
 }
